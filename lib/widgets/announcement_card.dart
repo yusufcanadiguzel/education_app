3ac248f3c068eb9e5540ce163bfe9b8b32df6770
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2023-12-22 23:06:23 +0300

    Create announcement_card.dart
    Duyurular kartı tasarlandı.

---
+++ lib/widgets/announcement_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class AnnouncementCard extends StatelessWidget {
+  const AnnouncementCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ClipPath(
+        child: Container(
+          decoration: const BoxDecoration(
+            border: Border(
+              left: BorderSide(color: Colors.green, width: 10),
+            )
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Duyuru', style: TextStyle(color: Color(0xFF00956e),),),
+                    Text('İstanbul Kodluyor', style: TextStyle(color: Color(0xFF00956e),),)
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: Text(
+                        'Yeni Grupların Discord\'a Katılımı',
+                        style: TextStyle(
+                            color: Color(0xFF767676),
+                            fontSize: 14.0
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Baseline(baseline: 110, baselineType: TextBaseline.alphabetic, child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Icon(Icons.calendar_month, color: Color(0xFF9397a0)),
+                          SizedBox(width: 5.0,),
+                          Text('22.12.2023', style: TextStyle(color: Color(0xFF9397a0)),)
+                        ],
+                      ),
+                    ),
+                    Text('Devamını Oku', style: TextStyle(color: Color(0xFF9397a0)),)
+                  ],
+                ),)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
